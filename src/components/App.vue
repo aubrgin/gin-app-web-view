a56f0b6e215f4de5143d552d8274264f96de54ae
decaruju <julien.decarufel@gmail.com>
2019-10-14 15:26:00 -0400

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="gin-app-web-view">
+    <template v-if="computedUrl">
+      <webview :src="computedUrl" />
+    </template>
+    <template v-else>
+      <gin-input v-model="tempUrl" />
+      <gin-button @click="dataUrl = tempUrl">
+        Load Page
+      </gin-button>
+    </template>
+  </div>
+</template>
+
+
+<script>
+import { GinInput, GinButton } from '@aubrgin/gin-components';
+
+export default {
+   name: 'WebView',
+   components: {
+     GinInput,
+     GinButton,
+   },
+   props: {
+     url: {
+       type: String,
+       default: '',
+     },
+   },
+   computed: {
+     computedUrl() {
+       return this.url || this.dataUrl;
+     },
+   },
+   data() {
+     return {
+       dataUrl: '',
+       tempUrl: '',
+     };
+   },
+}
+</script>
+
+<style lang="scss">
+ .gin-app-web-view {
+   height: 100%;
+   width: 100%;
+   overflow-x: hidden;
+
+   webview {
+     height: 100%;
+     width: 100%;
+   }
+ }
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import App from './components/App.vue';
+import ginFs from '@aubrgin/gin-fs';
+import fs from 'fs';
+import Vue from 'vue';
+
+if (process.env.NODE_ENV !== 'production') {
+  new Vue({
+    render: (h) => h(App),
+  }).$mount('#app');
+
+  function injectCss(css) {
+    var linkElement = document.createElement('link');
+    linkElement.setAttribute('rel', 'stylesheet');
+    linkElement.setAttribute('type', 'text/css');
+    linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent(css));
+    document.getElementsByTagName('head')[0].appendChild(linkElement);
+  }
+
+  injectCss(fs.readFileSync(`${ginFs.ginPath}/${ginFs.getConfig('theme', 'gin')}`));
+}
+
+
+
+
+export default App;
